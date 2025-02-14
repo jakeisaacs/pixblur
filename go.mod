5582936f0f6bec40dbfba10ead7fe957d5304c20
jisaacs <jakehisaacs@gmail.com>
2025-02-13 22:27:34 -0500

    Working on adding image info to db for now... Need gcc to run

--- go.mod
+++ go.mod
@@ -2,6 +2,9 @@ module pixblur.jkaisix
 
 go 1.23.2
 
-require github.com/disintegration/imaging v1.6.2
+require (
+	github.com/disintegration/imaging v1.6.2
+	github.com/mattn/go-sqlite3 v1.14.24
+)
 
 require golang.org/x/image v0.23.0 // indirect
--- internal/models/game_setup.go
+++ internal/models/game_setup.go
@@ -1,7 +1,63 @@
-package models
+// CHANGE TO "models" ONCE BETTER CONFIGURED
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	_ "github.com/mattn/go-sqlite3"
+)
 
 type GameSetup struct {
 	Name       string
 	Data       string
 	Date_Valid string
+}
+
+// CHANGE FUNCTION NAME ONCE BETTER CONFIGURED
+func main() {
+	name := "wizard"
+	valid_date := time.Now().Format("01/02/2006")
+
+	db, err := sql.Open("sqlite3", "./sqlite.db")
+	if err != nil {
+		fmt.Errorf("Unable to open db: %v", err)
+	}
+	defer db.Close()
+
+	insertImage := func() error {
+		imagePath := "./ui/static/img/wizard.png"
+		imageData, err := os.ReadFile(imagePath)
+
+		// fmt.Printf("name: %s, valid_date: %s\nData: ", name, valid_date, imageData)
+		if err != nil {
+			return fmt.Errorf("Error reading image: %v", err)
+		}
+
+		stmt, err := db.Prepare(`INSERT INTO game_info
+						(name, data, valid_date)
+						VALUES (?, ?, ?)`)
+		if err != nil {
+			return fmt.Errorf("Error preparing statement: %v", err)
+		}
+		defer stmt.Close()
+
+		_, err = stmt.Exec(name, imageData, valid_date)
+		if err != nil {
+			return fmt.Errorf("Error inserting image: %v", err)
+		}
+
+		return nil
+	}
+
+	err = insertImage()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully inserted image!")
+
 }
